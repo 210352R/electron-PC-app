d087376c27159047a41939b029d8edda8fe8bc18
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-11 01:58:17 +0530

    update index html

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  });
+
+  mainWindow.loadURL(startUrl); // load the index.html file into the window
+
+  mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createMainWindow);
