abba253cd1587d171c20188b3095a6470dcbcdf0
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-11 10:43:24 +0530

    add 'ipcRenderer' method in preload js

--- preload.js
+++ preload.js
@@ -5,3 +5,10 @@ contextBridge.exposeInMainWorld('electron', {
   homeDir: () => os.homedir(),
   osVersion: () => os.arch(),
 });
+
+// handle the event from the renderer process
+contextBridge.exposeInMainWorld('ipcRenderer', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, func) =>
+    ipcRenderer.on(channel, (event, ...args) => func(event, ...args)),
+});
